382e1fdea92caa120714565525bc8b132e20855f
Mahdinaghikhani <mahdinkh7@gmail.com>
2022-06-06 12:40:48 +0430

    Initial Commit

---
+++ learing/learingApp.swift
@@ -0,0 +1,17 @@
+//
+//  learingApp.swift
+//  learing
+//
+//  Created by mahdi naghikhani on 06/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct learingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
