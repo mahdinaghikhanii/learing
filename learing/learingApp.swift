f0e432c7fc0e813854d0ded35a311cfc2872993d
Mahdinaghikhani <mahdinkh7@gmail.com>
2022-06-07 18:20:50 +0430

    add image in assets

--- learing/learingApp.swift
+++ learing/learingApp.swift
@@ -1,17 +1,11 @@
-//
-//  learingApp.swift
-//  learing
-//
-//  Created by mahdi naghikhani on 06/06/2022.
-//
-
 import SwiftUI
 
 @main
 struct learingApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+     
         }
+        
     }
 }
